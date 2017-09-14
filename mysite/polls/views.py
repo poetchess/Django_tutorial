71a3870ed1cce586a5c6c1f07a0b67fda17a4d47
poetchess <ilvbaicai@126.com>
2017-09-14 18:25:57 +0800

    tutorial part 5: Functional test continued.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
+import traceback
 
 from .models import Question, Choice
 
@@ -23,6 +24,12 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
