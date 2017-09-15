0fcb1c3f245bcaae5b1e918e838f0d2809debf8a
poetchess <ilvbaicai@126.com>
2017-09-15 14:04:48 +0800

    tutorial part 7: Customize admin page: form, related objects, change list, look and feel, index page

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -17,12 +17,16 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('data published')
 
-    def __repr__(self):
+    def __str__(self):
         return self.question_text
 
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
@@ -30,5 +34,5 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    def __repr__(self):
+    def __str__(self):
         return self.choice_text
